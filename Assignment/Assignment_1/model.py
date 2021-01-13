0a6be802464f594140567b1f878aa2164f27f702
Gunu <gunu9441@gmail.com>
2021-01-13 13:50:25 +0900

    add hidden layers belonging to relus

--- Assignment/Assignment_1/model.py
+++ Assignment/Assignment_1/model.py
@@ -1,8 +1,20 @@
 import torch.nn as nn
 
+# class LinearClassifier(nn.Module):
+#     def __init__(self):
+#         super(LinearClassifier, self).__init__()
+#         self.layer_1 = nn.Linear(784, 10)
+#     def forward(self,x):
+#         return self.layer_1(x) 
+
 class LinearClassifier(nn.Module):
     def __init__(self):
         super(LinearClassifier, self).__init__()
-        self.layer_1 = nn.Linear(784, 10)
+        self.layer_1 = nn.Linear(784, 392)
+        self.layer_2 = nn.Linear(392,196)
+        self.layer_3 = nn.Linear(196,10)
     def forward(self,x):
-        return self.layer_1(x) 
+        x = nn.ReLU(self.layer_1(x))
+        x = nn.ReLU(self.layer_2(x))
+        x = self.layer_3
+        return x

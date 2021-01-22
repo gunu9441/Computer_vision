4925ac05cfc3118cb208707a205c35d503f13702
Gunu <gunu9441@gmail.com>
2021-01-22 22:48:53 +0900

    update code

--- python_basic/class.py
+++ python_basic/class.py
@@ -1,16 +1,3 @@
-# import torch
-# import torch.nn as nn
-
-
-# class MultivariateLinearRegressionModel(nn.Module):
-#     def __init__(self):
-#         # super().__init__()
-#         self.linear = nn.Linear(3,1)
-
-#     def forward(self,x):
-#         return self.linear(x)
-
-# model = MultivariateLinearRegressionModel();
 class Country:
     """Super Class"""
 
